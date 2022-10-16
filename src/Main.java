943f4c9f9300cb06f4f8e538387efcd12fff6d92
Andrey Kaizer <andre.kaizer@yandex.ru>
2022-10-17 00:42:18 +0300

    Доработка класса Main

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,60 @@
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+
 
 public class Main {
 
+    private static final String TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor" +
+            "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco" +
+            "laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate" +
+            "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident," +
+            "sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
     public static void main(String[] args) {
-        String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
-                " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." +
-                " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris " +
-                "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit" +
-                " in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint " +
-                "occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-        char[] chars = text.toCharArray();
-        Map<Character, Integer> map = new HashMap<>();
+        char[] kitSymbols = TEXT
+                .toLowerCase(Locale.ROOT)
+                .toCharArray();
+        Map<Character, Integer> symbols = new HashMap<>();
 
-        for (char ch : chars) {
-            if (map.containsKey(ch)) {
-                int value = map.get(ch) + 1;
-                map.replace(ch, value);
-            } else {
-                map.put(ch, 1);
-            }
-        }
-        // вывод всей map
-        System.out.println(map);
-        // max_VALUE
-        int maxValue = -1;
-        for (int value : map.values()) {
-            if (value > maxValue) {
-                maxValue = value;
-
+        for (Character currentSymbols : kitSymbols) {
+            if (currentSymbols != ' ') {
+                symbols.merge(currentSymbols, 1, (сurrentValue, defaultValue) -> ++сurrentValue);
             }
         }
 
-        System.out.println("Максимальное значение " + maxValue);
-        // min_VALUE
-        int minValue = Integer.MAX_VALUE;
-        for (int value : map.values()) {
-            if (value < minValue) {
-                minValue = value;
 
+        Map.Entry<Character, Integer> maxSymbol = getMaxSymbol(symbols);
+        Map.Entry<Character, Integer> minSymbol = getMinSymbol(symbols);
+
+        System.out.printf("Чаще всего встречается символ: %c количество %d\n", +maxSymbol.getKey(), maxSymbol.getValue());
+        System.out.printf("Реже всего встречается символ: %c количество %d  ", +minSymbol.getKey(), minSymbol.getValue());
+    }
+
+    private static Map.Entry<Character, Integer> getMaxSymbol(Map<Character, Integer> symbols) {
+        int maxCount = -1;
+        Map.Entry<Character, Integer> resultEntry = null;
+
+        for (Map.Entry<Character, Integer> currentEntry : symbols.entrySet()) {
+            if (currentEntry.getValue() > maxCount) {
+                resultEntry = currentEntry;
+                maxCount = currentEntry.getValue();
             }
         }
-        System.out.println("Минимальное значение " + " " + minValue);
 
+        return resultEntry;
+    }
+
+    private static Map.Entry<Character, Integer> getMinSymbol(Map<Character, Integer> symbols) {
+        int minCount = Integer.MAX_VALUE;
+        Map.Entry<Character, Integer> resultEntry = null;
+
+        for (Map.Entry<Character, Integer> currentEntry : symbols.entrySet()) {
+            if (currentEntry.getValue() < minCount) {
+                resultEntry = currentEntry;
+                minCount = currentEntry.getValue();
+            }
+        }
+        return resultEntry;
     }
 }
